a72931cf1c045a0db28b749998587da5e463dd5a
rigpadorji <gselrigdhen@gmail.com>
2024-03-24 11:30:18 +0900

    renamefile

--- javascript/indexdisplay.js
+++ javascript/indexdisplay.js
@@ -12,7 +12,19 @@ var year = document.getElementById("year").value;
  var dobdate = document.getElementById("dobdate").value;
   var male = document.getElementById("male").value;
  var female = document.getElementById("female").value;
-var display = "resume.html?year=" +  encodeURIComponent(year) +"&month=" + encodeURIComponent(month)+ "&date="+ encodeURIComponent(date)+ "&furigana=" +encodeURIComponent(furigana)+ "&romaji=" +encodeURIComponent(romaji)+"&dobyear="+ encodeURIComponent(dobyear)+"&dobmonth=" + encodeURIComponent(dobmonth)+ "&dobdate=" + encodeURIComponent(dobdate)+"&male=" + encodeURIComponent(male)+ "&female="+ encodeURIComponent(female);
+ var addinfurigana = document.getElementById("addinfurigana").value;
+  var addinromaji = document.getElementById("addinromaji").value;
+  var phone = document.getElementById("phone").value;
+   var email = document.getElementById("email").value;
+   var collegeyear1 = document.getElementById("collegeyear1").value;
+   var collegemonth1 = document.getElementById("collegemonth1").value;
+   var collegeyear2 = document.getElementById("collegeyear2").value;
+   var collegemonth2 = document.getElementById("collegemonth2").value;
+   var collegename =document.getElementById("collegename").value;
+
+
+var display = "resume.html?year=" +  encodeURIComponent(year) +"&month=" + encodeURIComponent(month)+ "&date="+ encodeURIComponent(date)+ "&furigana=" +encodeURIComponent(furigana)+ "&romaji=" +encodeURIComponent(romaji)+"&dobyear="+ encodeURIComponent(dobyear)+"&dobmonth=" + encodeURIComponent(dobmonth)+ "&dobdate=" + encodeURIComponent(dobdate)+"&male=" + encodeURIComponent(male)+ "&female="+ encodeURIComponent(female)+"&addinfurigana="+ encodeURIComponent(addinfurigana)+"&addinromaji=" +encodeURIComponent(addinromaji)+"&phone=" +encodeURIComponent(phone)+"&email="+encodeURIComponent(email)+"&collegeyear1=" +encodeURIComponent(collegeyear1) + "&collegemonth1=" +encodeURIComponent(collegemonth1)+"&collegeyear2=" +encodeURIComponent(collegeyear2) + "&collegemonth2=" +encodeURIComponent(collegemonth2)+"&collegename=" + encodeURIComponent(collegename);
+
 window.location.href=display;
  
 
